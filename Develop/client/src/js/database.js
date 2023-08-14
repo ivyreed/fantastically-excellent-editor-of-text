01df63924ec4d218389d7ae061c1547395573799
Ivy Reed <idcfu788@gmail.com>
2023-08-14 12:35:44 -0400

    first commit

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,40 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('feet', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('feet')) {
+        console.log('feet database already exists');
+        return;
+      }
+      db.createObjectStore('feet', { keyPath: 'id', autoIncrement: true });
+      console.log('feet database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {  
+  console.log("GET all from the database");
+
+// Create a connection to the database database and version we want to use.
+const feetDb = await openDB("feet", 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = feetDb.transaction("feet", "readonly");
+
+// Open up the desired object store.
+const store = tx.objectStore("feet");
+
+// Use the .getAll() method to get all data in the database.
+const request = store.getAll();
+
+// Get confirmation of the request.
+const result = await request;
+console.log("result.value", result);
+return result;
+};
+
+initdb();
---
+++ Develop/client/src/js/header.js
@@ -0,0 +1,13 @@
+const header = `
+/*
+    ________________________
+   / ____/ ____/ ____/_  __/
+  / __/ / __/ / __/   / /
+ / /   / /___/ /___  / /
+/_/   /_____/_____/ /_/
+
+Fantastically Excellent Editor of Text
+*/                          
+`;
+
+export { header };
